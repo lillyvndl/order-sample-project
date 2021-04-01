a6d3519d25ed8a9ed944024ec3207670cea2ad5f
Aurélie Vandaele <aurelie.vndl@gmail.com>
2021-04-01 15:36:51 +0200

    Provide method to get the order data

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    /**
+     * Get the order from a file on the server
+     */
+    public function get()
+    {
+        return file_get_contents(base_path('order.json'));
+    }
 }
